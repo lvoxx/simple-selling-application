1eca3e7c6449e515c851b34f5eed3d3926b5dedc
lvoxx <lvoxxartist@gmail.com>
2025-02-27 16:17:36 +0700

    Added test container

---
+++ src/test/java/com/shitcode/demo1/testcontainer/AbstractTestContainer.java
@@ -0,0 +1,58 @@
+package com.shitcode.demo1.testcontainer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.api.model.HostConfig;
+import com.github.dockerjava.api.model.PortBinding;
+import com.github.dockerjava.api.model.Ports;
+
+@SuppressWarnings("resource")
+@Testcontainers
+public abstract class AbstractTestContainer {
+    @Container
+    @ServiceConnection
+    protected static PostgreSQLContainer<?> postgreSQLContainer;
+ 
+    static {
+        int containerPort = 5432;
+        int localPort = 4321;
+        DockerImageName postgres = DockerImageName.parse("postgres:15.9");
+        postgreSQLContainer = new PostgreSQLContainer<>(postgres)
+                .withDatabaseName("test")
+                .withUsername("root")
+                .withPassword("root")
+                .withReuse(true)
+                .withExposedPorts(containerPort)
+                .withCreateContainerCmdModifier(cmd -> cmd.withHostConfig(
+                        new HostConfig().withPortBindings(
+                                new PortBinding(Ports.Binding.bindPort(localPort), new ExposedPort(containerPort)))));
+    }
+ 
+    @BeforeAll
+    static void startContainer() {
+        postgreSQLContainer.start();
+    }
+ 
+    @AfterAll
+    static void stopContainer() {
+        if (postgreSQLContainer != null) {
+            postgreSQLContainer.close();
+        }
+    }
+ 
+    @Test
+    void canEstablishedPostgreSqlContainer() {
+        assertThat(postgreSQLContainer.isCreated()).isTrue();
+        assertThat(postgreSQLContainer.isRunning()).isTrue();
+    }
+}
