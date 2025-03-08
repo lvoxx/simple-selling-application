d8237079bdfaae2a057957f8a28035d414c0f48a
lvoxx <lvoxxartist@gmail.com>
2025-03-08 18:53:59 +0700

    Added exception redirect

--- src/main/java/com/shitcode/demo1/controller/AuthController.java
+++ src/main/java/com/shitcode/demo1/controller/AuthController.java
@@ -1,10 +1,14 @@
 package com.shitcode.demo1.controller;
 
+import java.time.LocalDateTime;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shitcode.demo1.annotation.logging.LogCollector;
@@ -12,11 +16,16 @@ import com.shitcode.demo1.dto.AuthDTO;
 import com.shitcode.demo1.dto.ResponseDTO;
 import com.shitcode.demo1.dto.SpringUserDTO;
 import com.shitcode.demo1.exception.model.ErrorModel;
+import com.shitcode.demo1.exception.model.TokenExpiredException;
+import com.shitcode.demo1.exception.model.UserDisabledException;
+import com.shitcode.demo1.properties.FontendServerConfigData;
 import com.shitcode.demo1.properties.RateLimiterConfigData;
 import com.shitcode.demo1.service.AuthService;
 import com.shitcode.demo1.service.ResponseService;
 import com.shitcode.demo1.service.SpringUserService;
+import com.shitcode.demo1.utils.LogPrinter;
 import com.shitcode.demo1.utils.RateLimiterPlan;
+import com.shitcode.demo1.utils.LogPrinter.Type;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -25,10 +34,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.Data;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Data
 @RestController
@@ -41,13 +50,16 @@ public class AuthController {
         private final SpringUserService springUserService;
         private final ResponseService responseService;
         private final RateLimiterConfigData rateLimiterConfigData;
+        private final FontendServerConfigData fontendServerConfigData;
 
         public AuthController(AuthService authService, SpringUserService springUserService,
-                        ResponseService responseService, RateLimiterConfigData rateLimiterConfigData) {
+                        ResponseService responseService, RateLimiterConfigData rateLimiterConfigData,
+                        FontendServerConfigData fontendServerConfigData) {
                 this.authService = authService;
                 this.springUserService = springUserService;
                 this.responseService = responseService;
                 this.rateLimiterConfigData = rateLimiterConfigData;
+                this.fontendServerConfigData = fontendServerConfigData;
         }
 
         private RateLimiterPlan LOGIN_PLAN;
@@ -103,14 +115,28 @@ public class AuthController {
                         @ApiResponse(responseCode = "200", description = "Active user successfully", content = @Content(mediaType = "application/vnd.lvoxx.app-v1+json", schema = @Schema(implementation = ResponseDTO.class))),
                         @ApiResponse(responseCode = "400", description = "Registration token not found", content = @Content(mediaType = "application/vnd.lvoxx.app-v1+json", schema = @Schema(implementation = ErrorModel.class))),
                         @ApiResponse(responseCode = "410", description = "Registration token is expired", content = @Content(mediaType = "application/vnd.lvoxx.app-v1+json", schema = @Schema(implementation = ErrorModel.class))),
+                        @ApiResponse(responseCode = "422", description = "User is locked", content = @Content(mediaType = "application/vnd.lvoxx.app-v1+json", schema = @Schema(implementation = ErrorModel.class))),
                         @ApiResponse(responseCode = "429", description = "Too many request", content = @Content(mediaType = "application/vnd.lvoxx.app-v1+json", schema = @Schema(implementation = ErrorModel.class)))
         })
         @GetMapping(path = "#{@mailingConfigData.getRegisterEmail().getPath()}", produces = "application/vnd.lvoxx.app-v1+json")
-        public ResponseEntity<?> activeUserV1(@RequestParam String token) throws Exception {
-                return responseService.mapping(
-                                () -> new ResponseEntity<>(springUserService.activeUserAccount(token),
-                                                HttpStatus.OK),
-                                COMPLETE_SIGNUP);
+        public void activeUserV1(@RequestParam String token, HttpServletRequest request, HttpServletResponse response)
+                        throws Exception {
+                try {
+                        responseService.mapping(
+                                        () -> new ResponseEntity<>(springUserService.activeUserAccount(token),
+                                                        HttpStatus.OK),
+                                        COMPLETE_SIGNUP);
+                } catch (UserDisabledException | TokenExpiredException e) {
+                        if (e instanceof UserDisabledException) {
+                                response.sendRedirect(fontendServerConfigData.getActive().get("disabled"));
+                        } else if (e instanceof TokenExpiredException) {
+                                response.sendRedirect(fontendServerConfigData.getActive().get("expired"));
+                        }
+                        LogPrinter.printControllerLog(Type.ERROR, request.getContextPath(), "AuthController",
+                                        "activeUserV1", LocalDateTime.now().toString(), e.getMessage());
+                        response.sendRedirect(fontendServerConfigData.getUnknown().concat(e.getMessage()));
+                }
+                response.sendRedirect(fontendServerConfigData.getActive().get("success"));
         }
 
 }
---
+++ src/main/java/com/shitcode/demo1/properties/FontendServerConfigData.java
@@ -0,0 +1,18 @@
+package com.shitcode.demo1.properties;
+
+import java.util.Map;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "fontend-server")
+public class FontendServerConfigData {
+    private String base;
+    private String unknown;
+    private Map<String, String> login;
+    private Map<String, String> active;
+}
