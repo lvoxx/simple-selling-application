8dcc105a21d84e8fa60a8fbb7426d178b09057b2
lvoxx <lvoxxartist@gmail.com>
2025-03-25 15:49:23 +0700

    Fixed datetime print

--- src/main/java/com/shitcode/demo1/helper/PrettyPrintList.java
+++ src/main/java/com/shitcode/demo1/helper/PrettyPrintList.java
@@ -1,24 +1,27 @@
 package com.shitcode.demo1.helper;
 
+import java.util.Arrays;
 import java.util.List;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public abstract class PrettyPrintList {
     public static void printPrettyJson(List<Object[]> data) {
         try {
             ObjectMapper objectMapper = new ObjectMapper();
             objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+            objectMapper.registerModule(new JavaTimeModule()); // Fix for Java 8 date/time serialization
 
             // Convert List<Object[]> to a List of Lists for JSON serialization
             List<List<Object>> prettyData = data.stream()
-                    .map(List::of)
+                    .map(Arrays::asList) // Correctly convert Object[] to List<Object>
                     .toList();
 
             // Print Pretty JSON
             String jsonOutput = objectMapper.writeValueAsString(prettyData);
-            System.out.println(jsonOutput);
+            System.out.println(jsonOutput.replace("], [", "],\n [")); // Force line breaks;
         } catch (Exception e) {
             e.printStackTrace();
         }
