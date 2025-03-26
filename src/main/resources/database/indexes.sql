04025e106d05edb5db6750ba53fe2cb7cb934d8e
lvoxx <lvoxxartist@gmail.com>
2025-03-26 14:19:57 +0700

    Fixed query

--- src/main/resources/database/indexes.sql
+++ src/main/resources/database/indexes.sql
@@ -6,23 +6,23 @@ ON products USING GIN (to_tsvector('english', name));
 CREATE INDEX IF NOT EXISTS idx_discount_title_btree
 ON discount(title);
 
-CREATE INDEX IF NOT EXISTS idx_discount_exp_date_brin
-ON discount(exp_date) USING brin(timestamp);
+CREATE INDEX IF NOT EXISTS idx_discount_exp_date_brin 
+ON discount USING BRIN (exp_date);
 
 -- Product Interaction
 CREATE INDEX IF NOT EXISTS idx_product_interaction_on_time_brin 
-ON product_interaction(on_time) USING brin(timestamp);
+ON product_interaction USING BRIN (on_time);
 
 CREATE INDEX IF NOT EXISTS idx_product_interaction_locate_at_btree 
 ON product_interaction(locate_at);
 
 -- Spring User
 CREATE INDEX IF NOT EXISTS idx_spring_user_email_hash 
-ON spring_user USING hash(email);
+ON spring_user USING HASH (email);
 
 CREATE INDEX IF NOT EXISTS idx_spring_user_phone_number_hash 
-ON spring_user USING hash(phone_number);
+ON spring_user USING HASH (phone_number);
 
 -- Registration Token
 CREATE INDEX IF NOT EXISTS idx_registration_token_token_hash 
-ON registration_token USING hash(token);
+ON registration_token USING HASH (token);
