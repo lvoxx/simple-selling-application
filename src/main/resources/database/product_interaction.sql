69d08b7d1bdf4f5d8dfd4838352eac2cb79aee0c
lvoxx <lvoxxartist@gmail.com>
2025-04-01 17:27:22 +0700

    Decrease ProItr to 50k rows

--- src/main/resources/database/product_interaction.sql
+++ src/main/resources/database/product_interaction.sql
@@ -6,6 +6,5 @@ SELECT
     CASE WHEN random() < 0.95 THEN md5(random()::text) ELSE 'Anonymous User' END,  
     NOW() - (interval '60 days' * random()),  
     CASE WHEN random() < 0.98 THEN md5(random()::text) ELSE 'Unknown' END  
-FROM generate_series(1, 100000);
+FROM generate_series(1, 10000);
 COMMIT;
--- Repeat this statement 90 times for 9 million rows
