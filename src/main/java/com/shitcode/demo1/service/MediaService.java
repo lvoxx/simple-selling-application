c2bd287d0da371e314a299e3d6f4d95ff28558c7
lvoxx <lvoxxartist@gmail.com>
2025-04-01 16:17:41 +0700

    Added multi save media

--- src/main/java/com/shitcode/demo1/service/MediaService.java
+++ src/main/java/com/shitcode/demo1/service/MediaService.java
@@ -2,6 +2,7 @@ package com.shitcode.demo1.service;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,6 +10,8 @@ import org.springframework.web.multipart.MultipartFile;
 import com.shitcode.demo1.service.impl.LocalMediaServiceImpl.TypeOfMedia;
 
 public interface MediaService {
+    List<String> saveMediaFiles(List<MultipartFile> files) throws Exception;
+
     String saveMediaFile(MultipartFile file) throws Exception;
 
     Resource findFile(String filePathAndNameWithExtension) throws FileNotFoundException;
